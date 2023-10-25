3de2e67177033aa26663882e8dfe0ab07af0d202
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-10-25 15:30:03 -0500

    Save base64 encoder history

--- src/app/tools/base64encoder/page.tsx
+++ src/app/tools/base64encoder/page.tsx
@@ -1,4 +1,8 @@
 import Bas64EncoderComponent from "@/app/tools/base64encoder/Base64EncoderClientComponent";
+import { getUserAndSubscriptionState } from "@/actions/user";
 
-const Base64Encoder = () => <Bas64EncoderComponent />;
+const Base64Encoder = async () => {
+  const { user, isProUser } = await getUserAndSubscriptionState();
+  return <Bas64EncoderComponent user={user} isProUser={isProUser} />;
+};
 export default Base64Encoder;
