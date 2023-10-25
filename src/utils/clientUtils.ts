3bd23e80438e99c0a5e4a5f2827fa8599cf8ccff
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-10-24 21:34:20 -0500

    Add basic clerk and db stuff

---
+++ src/utils/clientUtils.ts
@@ -0,0 +1,21 @@
+import { ToolType } from "@prisma/client";
+
+const saveHistory = async (
+  userId: string,
+  toolType: ToolType,
+  metadata: Record<string, any>,
+  onError: (error: string) => void
+) => {
+  const resp = await fetch("/api/history", {
+    method: "POST",
+    body: JSON.stringify({
+      userId,
+      toolType,
+      metadata: JSON.stringify(metadata),
+    }),
+  });
+  const data = await resp.json();
+  if (data.status === "error") {
+    onError("Error saving into your history");
+  }
+};
