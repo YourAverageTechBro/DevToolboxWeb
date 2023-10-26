d2ad2419981ea0fbb37254e5dde667f5de59312c
Dohyun Kim <dohyun@youraveragetechbro.com>
2023-10-25 19:46:22 -0500

    Update public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/tools(.*)",
+    "/successful-purchase",
+    "/api/webhooks/stripe",
+  ],
 });
 
 export const config = {
