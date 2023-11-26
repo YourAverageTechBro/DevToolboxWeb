b21385a3750c618538be144790183ff1553f4e09
nutchanon-c <nutchanon.char@gmail.com>
2023-11-26 20:04:13 +0700

    better styling

--- src/app/tools/qrcode-generator/QrCodeGeneratorComponent.tsx
+++ src/app/tools/qrcode-generator/QrCodeGeneratorComponent.tsx
@@ -39,16 +39,28 @@ export default function QrCodeGeneratorComponent({
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     const img = new Image();
+
     img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx!.drawImage(img, 0, 0);
+      const borderSize = 10;
+      const borderColor = "#ffffff";
+
+      canvas.width = img.width + 2 * borderSize;
+      canvas.height = img.height + 2 * borderSize;
+
+      ctx!.fillStyle = borderColor;
+      ctx!.fillRect(0, 0, canvas.width, canvas.height);
+
+      const xPosition = (canvas.width - img.width) / 2;
+      const yPosition = (canvas.height - img.height) / 2;
+      ctx!.drawImage(img, xPosition, yPosition);
+
       const pngFile = canvas.toDataURL("image/png");
       const downloadLink = document.createElement("a");
       downloadLink.download = "QRCode";
       downloadLink.href = `${pngFile}`;
       downloadLink.click();
     };
+
     img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
   };
 
@@ -76,19 +88,20 @@ export default function QrCodeGeneratorComponent({
           </button>
         </div>
         {qrText && (
-          <div className="mt-5">
-            <QRCode
-              size={256}
-              style={{ border: "5px solid white" }}
-              bgColor="white"
-              fgColor="black"
-              viewBox={`0 0 256 256`}
-              value={qrText}
-              id="QRCode"
-            />
+          <div className="mt-5 flex gap-5 items-end">
+            <div className="flex border-white border-4">
+              <QRCode
+                size={256}
+                bgColor="white"
+                fgColor="black"
+                viewBox={`0 0 256 256`}
+                value={qrText}
+                id="QRCode"
+              />
+            </div>
             <button
               type="button"
-              className="rounded-md mt-4 bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              className="h-11 rounded-md mt-4 bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               onClick={onImageDownload}
             >
               Download
