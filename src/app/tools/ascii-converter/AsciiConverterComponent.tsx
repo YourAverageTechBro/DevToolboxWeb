10c0aee8d21d16e1ff2c28371c662b18947c3286
nutchanon-c <nutchanon.char@gmail.com>
2023-11-26 17:17:13 +0700

    shorten clear button

--- src/app/tools/ascii-converter/AsciiConverterComponent.tsx
+++ src/app/tools/ascii-converter/AsciiConverterComponent.tsx
@@ -135,15 +135,15 @@ export default function AsciiConverterComponent({
           </button>
         </div>
       </div>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-        onClick={async () => {
-          reset();
-        }}
-      >
-        Clear
-      </button>
+      <div>
+        <button
+          type="button"
+          className="rounded-md bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          onClick={reset}
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
