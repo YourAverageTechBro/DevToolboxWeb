4b51f7cdd367b06abe9c18b7965194228a588f38
Sabbir Siddiqui <sabbir.m.siddiqui@gmail.com>
2023-11-30 12:32:46 +0600

    fix: update filter input padding to 4px

--- src/app/components/common/FormatedJsonOutput.tsx
+++ src/app/components/common/FormatedJsonOutput.tsx
@@ -142,7 +142,7 @@ export default function FormattedJsonOutput({
       </div>
 
       <pre
-        className="px-8 py-2 block rounded-lg border-0
+        className="px-4 py-2 block rounded-lg border-0
         bg-gray-700 shadow-sm ring-1 ring-inset
         ring-gray-300 focus:ring-2 focus:ring-inset
         focus:ring-indigo-600 sm:text-sm sm:leading-6 language-json whitespace-pre-wrap
