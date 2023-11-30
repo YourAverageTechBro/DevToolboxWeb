6c611e20e57c7179aa781d5538f078cf6bcb4b31
Sabbir Siddiqui <sabbir.m.siddiqui@gmail.com>
2023-11-30 12:28:32 +0600

    fix: format json after filtering

--- src/app/components/common/FormatedJsonOutput.tsx
+++ src/app/components/common/FormatedJsonOutput.tsx
@@ -82,6 +82,12 @@ export default function FormattedJsonOutput({
     }
   };
 
+  const setFormattedJson = (jsonString: string) => {
+    const formattedJson = formatJson(jsonString);
+    setOutput(formattedJson);
+    setDangerousHtml(syntaxHighlight(formattedJson));
+  };
+
   useEffect(() => {
     try {
       if (debouncedJsonPathFilter) {
@@ -91,17 +97,12 @@ export default function FormattedJsonOutput({
             jsonPathFilter
           )
         );
-        setOutput(filteredJson);
-        setDangerousHtml(syntaxHighlight(filteredJson));
+        setFormattedJson(filteredJson);
       } else {
-        const json = formatJson(value);
-        setOutput(json);
-        setDangerousHtml(syntaxHighlight(json));
+        setFormattedJson(value);
       }
     } catch (e: any) {
-      const json = formatJson(value);
-      setOutput(json);
-      setDangerousHtml(syntaxHighlight(json));
+      setFormattedJson(value);
     }
   }, [
     formatJson,
@@ -150,7 +151,7 @@ export default function FormattedJsonOutput({
         style={{ height: "calc(100% - 96px)" }}
       />
       <input
-        className={`mt-4 block w-full rounded-md border-0 py-1.5
+        className={`mt-4 px-8 block w-full rounded-md border-0 py-1.5
       text-white shadow-sm ring-1 ring-inset ring-gray-300
       placeholder:text-gray-400 focus:ring-2 focus:ring-inset
       focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-600`}
