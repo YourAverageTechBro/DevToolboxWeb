8bd631298a30bd7f62818faa659e75fdc8041587
Aaron Boushley <aaron@boushley.cc>
2023-12-15 11:21:51 -0800

    Remove unnecessary props

--- src/app/tools/uuid-generator/page.tsx
+++ src/app/tools/uuid-generator/page.tsx
@@ -1,8 +1,6 @@
 import UuidGeneratorComponent from "@/app/tools/uuid-generator/UuidGeneratorComponent";
-import { getUserAndSubscriptionState } from "@/actions/user";
 
 const UuidGeneratorPage = async () => {
-  const { user, isProUser } = await getUserAndSubscriptionState();
-  return <UuidGeneratorComponent user={user} isProUser={isProUser} />;
+  return <UuidGeneratorComponent />;
 };
 export default UuidGeneratorPage;
