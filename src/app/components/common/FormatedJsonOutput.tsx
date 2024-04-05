eeba46323648c81172ecf4246c49ef38ea1cf36d
Syed Shoeb <syedshoeb886@gmail.com>
2024-04-05 19:17:15 +0530

    fixing copy to clipboard feature

--- src/app/components/common/FormatedJsonOutput.tsx
+++ src/app/components/common/FormatedJsonOutput.tsx
@@ -106,7 +106,12 @@ export default function FormattedJsonOutput({
             type="button"
             className="rounded-md bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             onClick={async () => {
-              await navigator.clipboard.writeText(output);
+              const jsonOutput = JSON.stringify(
+                JSON.parse(output),
+                null,
+                numberOfSpaces
+              );
+              await navigator.clipboard.writeText(jsonOutput);
             }}
           >
             Copy
