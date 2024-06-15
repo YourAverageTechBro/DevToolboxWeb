0a2453d02b1cdb62b8cab7abfaac5d2642062089
nischay2x <77281955+nischay2x@users.noreply.github.com>
2024-06-15 10:39:23 +0000

    feat: added sidebar collapse for less than 'md' screen

--- src/app/components/common/ToolList.tsx
+++ src/app/components/common/ToolList.tsx
@@ -101,55 +101,78 @@ export const toolList: ToolOption[] = [
 export default function ToolList() {
   const pathname = usePathname();
   return (
-    <div className="w-72 bg-gray-700 flex shrink-0 flex-col overflow-y-scroll">
-      <SignedIn>
-        <div className={"px-2 my-4 flex justify-between w-full"}>
-          <UserButton afterSignOutUrl="/tools/json-validator" />
-          <Button intent={"secondary"} href={"/tools/history"}>
-            View History
-          </Button>
-        </div>
-      </SignedIn>
-      <SignedOut>
-        <div className={"mx-2 my-4 flex flex-col gap-4"}>
-          <Button fullWidth intent={"primary"} href={"/sign-in"}>
-            Log in / Sign-up
-          </Button>
-          <p className={"text-xs text-gray-200"}>
-            {" "}
-            You only have to create an account if you want to upgrade to
-            DevToolbox Pro which saves your history so you can keep track of all
-            the actions you have done.
-          </p>
-        </div>
-      </SignedOut>
-      <Link
-        className={`w-full border-y py-3 px-4 hover:bg-gray-600`}
-        href={`https://github.com/YourAverageTechBro/DevToolboxWeb`}
-        target="_blank"
+    <>
+      <label
+        htmlFor="sidebar-toggle"
+        className="bg-indigo-500 md:hidden text-white px-2 py-1 rounded-md hover:bg-indigo-600 cursor-pointer absolute top-2 right-2"
       >
-        <div className={"flex items-center gap-2 "}>
-          <StarIcon className={"w-6 h-6"} />
-          Star Us On Github
-        </div>
-      </Link>
-      {toolList
-        .sort((a, b) => {
-          if (a.name < b.name) return -1;
-          else if (a.name > b.name) return 1;
-          return 0;
-        })
-        .map((toolOption) => (
-          <Link
-            className={`w-full border-b py-3 px-4 hover:bg-gray-600 ${
-              pathname === toolOption.path && "bg-gray-500"
-            }`}
-            key={toolOption.name}
-            href={toolOption.path}
-          >
-            <p> {toolOption.name}</p>
-          </Link>
-        ))}
-    </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-menu"
+        >
+          <line x1="4" x2="20" y1="12" y2="12" />
+          <line x1="4" x2="20" y1="6" y2="6" />
+          <line x1="4" x2="20" y1="18" y2="18" />
+        </svg>
+      </label>
+      <input type="checkbox" id="sidebar-toggle" className="peer hidden" />
+      <div className="w-0 peer-checked:w-72 md:w-72 bg-gray-700 flex shrink-0 flex-col no-scrollbar overflow-y-scroll transition-all">
+        <SignedIn>
+          <div className={"px-2 my-4 flex justify-between w-full"}>
+            <UserButton afterSignOutUrl="/tools/json-validator" />
+            <Button intent={"secondary"} href={"/tools/history"}>
+              View History
+            </Button>
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <div className={"mx-2 my-4 flex flex-col gap-4"}>
+            <Button fullWidth intent={"primary"} href={"/sign-in"}>
+              Log in / Sign-up
+            </Button>
+            <p className={"text-xs text-gray-200"}>
+              You only have to create an account if you want to upgrade to
+              DevToolbox Pro which saves your history so you can keep track of
+              all the actions you have done.
+            </p>
+          </div>
+        </SignedOut>
+        <Link
+          className={`w-full border-y py-3 px-4 hover:bg-gray-600`}
+          href={`https://github.com/YourAverageTechBro/DevToolboxWeb`}
+          target="_blank"
+        >
+          <div className={"flex items-center gap-2 "}>
+            <StarIcon className={"w-6 h-6"} />
+            Star Us On Github
+          </div>
+        </Link>
+        {toolList
+          .sort((a, b) => {
+            if (a.name < b.name) return -1;
+            else if (a.name > b.name) return 1;
+            return 0;
+          })
+          .map((toolOption) => (
+            <Link
+              className={`w-full border-b py-3 px-4 hover:bg-gray-600 ${
+                pathname === toolOption.path && "bg-gray-500"
+              }`}
+              key={toolOption.name}
+              href={toolOption.path}
+            >
+              <p> {toolOption.name}</p>
+            </Link>
+          ))}
+      </div>
+    </>
   );
 }
--- src/app/tools/layout.tsx
+++ src/app/tools/layout.tsx
@@ -6,16 +6,10 @@ export default function ToolsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className={"h-full w-full flex gap-4"}>
-      {" "}
+    <div className={"h-full w-full flex"}>
       <ToolList />
-      <div
-        className={"pt-4 px-4 flex-1 overflow-x-hidden"}
-      >
-        <div style={{height: "calc(100% - 2rem)"}}>
-          {" "}
-          {children}{" "}
-        </div>
+      <div className={"pt-14 md:pt-4 px-4 flex-1 overflow-x-hidden"}>
+        <div style={{ height: "calc(100% - 2rem)" }}>{children}</div>
       </div>
     </div>
   );
